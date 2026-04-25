44d3231932526e122077a6a014659ac658bf5fec
sham-404 <shamsujith4@gmail.com>
2026-04-25 23:56:14 +0530

    fen parsing tested with multiple perft, tested ok

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,41 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use roxie::{board::Board, perft::perft};
+    const FEN: &str = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1 ";
+
+    static DEPTH_1: u64 = 6;
+    static DEPTH_2: u64 = 264;
+    static DEPTH_3: u64 = 9467;
+    static DEPTH_4: u64 = 422333;
+    static DEPTH_5: u64 = 15833292;
 
     #[test]
     fn perft_1() {
-        let mut board = Board::start_pos();
-        assert_eq!(perft(&mut board, 1), 20);
+        let mut board = Board::load_fen(FEN);
+        assert_eq!(perft(&mut board, 1), DEPTH_1);
     }
 
     #[test]
     fn perft_2() {
-        let mut board = Board::start_pos();
-        assert_eq!(perft(&mut board, 2), 400);
+        let mut board = Board::load_fen(FEN);
+        assert_eq!(perft(&mut board, 2), DEPTH_2);
     }
 
     #[test]
     fn perft_3() {
-        let mut board = Board::start_pos();
-        assert_eq!(perft(&mut board, 3), 8902);
+        let mut board = Board::load_fen(FEN);
+        assert_eq!(perft(&mut board, 3), DEPTH_3);
     }
 
     #[test]
     fn perft_4() {
-        let mut board = Board::start_pos();
-        assert_eq!(perft(&mut board, 4), 197_281);
+        let mut board = Board::load_fen(FEN);
+        assert_eq!(perft(&mut board, 4), DEPTH_4);
     }
 
     #[test]
     fn perft_5() {
-        let mut board = Board::start_pos();
-        assert_eq!(perft(&mut board, 5), 4_865_609);
+        let mut board = Board::load_fen(FEN);
+        assert_eq!(perft(&mut board, 5), DEPTH_5);
     }
 }
