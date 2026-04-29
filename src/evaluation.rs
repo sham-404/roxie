ef56f5079c00c49689e6ccb1d54c8fc4275e3896
sham-404 <shamsujith4@gmail.com>
2026-04-29 07:04:03 +0530

    Deleted evalution.rs for now

--- src/evaluation.rs
+++
@@ -1,5 +0,0 @@
-use crate::{board::Board};
-
-pub fn evaluate(board: &Board) -> usize {
-    0
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 pub mod board;
 pub mod r#const;
 pub mod items;
-pub mod square;
 pub mod perft;
-pub mod uci;
 pub mod search;
-pub mod evaluation;
+pub mod square;
+pub mod uci;
